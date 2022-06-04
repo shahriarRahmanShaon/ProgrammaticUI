7fb8f9eb2182dcb9cbd2df1a6484c42ef515f3bb
shahriarRahmanShaon <shaon463@gmail.com>
2022-06-04 12:40:33 +0600

    Initial Commit

---
+++ ProgrammaticUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProgrammaticUI
+//
+//  Created by sergio shaon on 4/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
